ef4658b8bfbbcc73a4d8b1b03cb8705ac4b94489
wren27 <40725205+wren27@users.noreply.github.com>
2018-07-01 17:27:07 -0500

    Add files via upload

---
+++ C++sample.cpp
@@ -0,0 +1,28 @@
+# include <iostream>
+#include <iomanip >
+using namespace std ;
+int main ()
+{
+	
+int alpha[50];
+for (int i=0;i<25;i++)
+{
+alpha[i]=i*i;
+cout<<alpha[i]<<" ";
+}
+for(int i=25;i<50;i++)
+{
+alpha[i]=3*i;
+cout<<alpha[i]<<" ";
+}
+for(int i=1 ; i<51 ; i++)    {                           
+    {
+            
+            cout<<setw(3)<<alpha[i-1]<<" ";
+            if( (i+1)%10 == 0)
+            cout<<endl;
+    }
+    
+}
+return 0;
+}
